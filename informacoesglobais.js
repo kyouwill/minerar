9846de6f34636dcad34ca67864b227576cfe2e56
kyouwill <vinicius.pereira.garcia@escola.pr.gov.br>
2025-09-01 09:44:49 -0300

    Update informacoesglobais.js

--- informacoesglobais.js
+++ informacoesglobais.js
@@ -1,9 +1,21 @@
 const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json"
 
-async function inforgloba(params) {
+async function visualizarinformacao(params) {
     const res = await fetch(url)
     const dados = await res.json()
     console.log(dados);
+
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const mediaDeTempo = parseInt(dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio - mediaDeTempo) * 100)
+    const pessoasConectadasNoMundo = (dados.total_pessoas_conectadas / 1e9)
+
+
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container_texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo}</span> de pessoas e aproximadamente <span>${pessoasConectadasNoMundo}</span> estão conectadas em alguma rede social e passam em média <span>${mediaDeTempo}</span></span>:<span>${minutos}</span>`
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
 }
 
-inforgloba();
+visualizarinformacao();
